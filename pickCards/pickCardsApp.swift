7f33eeb814cc149364e5975b7aa9e2a8199e9a3f
shahriarRahmanShaon <shaon463@gmail.com>
2021-01-06 21:38:25 +0600

    Initial Commit

---
+++ pickCards/pickCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  pickCardsApp.swift
+//  pickCards
+//
+//  Created by sergio shaon on 6/1/21.
+//
+
+import SwiftUI
+
+@main
+struct pickCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
